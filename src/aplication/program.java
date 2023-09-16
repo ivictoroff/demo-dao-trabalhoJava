f8da3fe2ef2ec86c44b8efd44490e8b9d7d66af5
ivictoroff <victoraraujosantos976@gmail.com>
2023-09-16 20:39:30 -0300

    classe de clientes

---
+++ src/aplication/program.java
@@ -0,0 +1,13 @@
+package aplication;
+
+import model.entities.Cliente;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Cliente obj = new Cliente(1, "victor");
+		System.out.println(obj);
+	}
+
+}
